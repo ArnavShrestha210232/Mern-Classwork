4a7e48028e413dd0c66720f4c6a25544cc13bc70
ArnavShrestha210232 <101889297+ArnavShrestha210232@users.noreply.github.com>
2023-12-18 12:00:53 +0545

    Node Homework

---
+++ Node3/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'; // Added import statement for React
+import { useState } from 'react';
+
+
+
+// const App = () => {
+//   const [counter, setCounter] = useState(5)
+  
+//   setTimeout(
+//     () => setCounter(counter + 15),
+    
+//     1000)
+//   return <h1>{counter}</h1>
+// }
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  
+  // setTimeout(
+  //   () => setCounter(counter + 15),
+    
+  //   1000)
+  const handleClick = () => {
+    setCounter(counter+1)
+  }
+  return (
+    <>
+      <div>{counter}</div>
+      <hr/>
+        <button onClick={handleClick} >Click Me</button>
+    </>
+  
+  )
+}
+
+export default App
