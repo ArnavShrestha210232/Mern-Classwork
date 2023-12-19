24b0c4dac01aa103049b9d062678ec448919064f
ArnavShrestha210232 <101889297+ArnavShrestha210232@users.noreply.github.com>
2023-12-19 08:38:50 +0545

    Week2 Event handling

--- Node4/src/App.jsx
+++ Node4/src/App.jsx
@@ -7,14 +7,24 @@ const App=() => {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+
+    const descreaseByOne = () => setCounter(counter - 1)
+
+
+
+  const setToSeven=()=> setCounter(7)
   
   const setToZero=()=> setCounter(0)
   return (
     <div>
       <div>{counter}</div>
       <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToSeven}>SEVEN</button>
     
-    <button onClick={setToZero}>zero</button>
+      <button onClick={setToZero}>zero</button>
+      
+      <button onClick={descreaseByOne}>minus
+      </button>
     </div>
   )
 }
